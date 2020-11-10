8aa24e12f3d2267e2f441d8c0d232a0d45cd76cc
uTen2c <uten2c@gmail.com>
2020-11-10 19:08:40 +0900

    Initial commit

---
+++ src/main/java/org/syringemc/syringeclient/SyringeClient.java
@@ -0,0 +1,11 @@
+package org.syringemc.syringeclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SyringeClient implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
