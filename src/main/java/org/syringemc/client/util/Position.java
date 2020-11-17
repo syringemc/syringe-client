7dfd0efa8d79e9fcf9f9af146a199f2aaf4f8ea6
uTen2c <uten2c@gmail.com>
2020-11-17 19:20:49 +0900

    Add Position.java

---
+++ src/main/java/org/syringemc/client/util/Position.java
@@ -0,0 +1,13 @@
+package org.syringemc.client.util;
+
+public enum Position {
+    TOP,
+    BOTTOM,
+    LEFT,
+    RIGHT,
+    TOP_LEFT,
+    TOP_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_RIGHT,
+    CENTER
+}
